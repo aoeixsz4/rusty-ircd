bab157505d39183b52e64c84acc22dc7188272d0
John Doyle <jjadoyle@gmail.com>
2019-07-29 08:18:04 +0200

    in the process of testing irc::parse_message() after substantial rehaul

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 mod irc;
+use irc::*;
+use std::io;
 
 fn main () {
-	let mut msg_buf = irc::MessageBuffer::new();
-	if let Err(_) = msg_buf.append(String::from("JOIN ##rust\r\n")) {
-		println!("received an error!");
-	}
-	if let Err(_) = msg_buf.append(String::from("MSG ##rust Hello, guys\r\n")) {
-		println!("received an error!");
-	}
-	if let Err(_) = msg_buf.append(String::from("LOLOL OMG WUT\r\n")) {
-		println!("received an error!");
-	}
-	println!("msg_buf.index is {}", msg_buf.index);
-	loop {
-		if let Some(string) = msg_buf.extract() {
-			println!("{}", string);
-		} else {
-			break;
-		}
-	}
+    let mut irc_msg = String::new();
+    loop {
+        io::stdin().read_line(&mut irc_msg)
+            .expect("Failed to read line");
+        
+        //irc_msg.extend();
+        if let Ok(irc_msg, source) = extract_prefix(irc_msg) {
+            match source {
+                Source::Server(server_name) => println!("you are server: {}", server_name);
+                Source::User(nick, user, host) => {
+                    println!("Hi, {}!", nick);
+                    if let Some(name) = user {
+                        println!("Your username is {}", name);
+                    }
+                    if let Some(name) = host {
+                        println!("Your hostname is {}", host);
+                    }
+                }
+            }
+        }
+    }
 }
 
