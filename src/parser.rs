781721e43e544cc539e3ab36b07c29801e5ae27b
John Doyle <jjadoyle@gmail.com>
2019-07-31 03:20:52 +0200

    refactored a bunch of parsing code, and also made sure to do tests, the new scheme for parse_message() is mostly working nicely, but we need to modify parse_prefix() a lot, and implement sanity checks in irc/rfc_defs.rs like is_valid_command() and others

--- src/parser.rs
+++ src/parser.rs
@@ -7,22 +7,172 @@
 // irc::command or so)
 // link: https://tools.ietf.org/html/rfc2812#section-2.3.1
 // plus an optional source field (for server messages, indicating origin)
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr}
 use crate::irc;
 use crate::irc::rfc_defs as rfc;
 
 // will want to change these types at some point
 #[derive(Debug)]
 pub enum ParseError {
-    CommandNotRecognised(String),
-    TooFewArgs(String),
-    MissingCRLF,
     InvalidPrefix,
     NoCommand,
-    EmptyLine
+    InvalidCommand
 }
 
-// 
-fn parse_prefix(msg: &str) -> Result<Box<irc::Source>, ParseError> {
+pub enum HostType {
+    HostName(String),
+    HostAddr(IpAddr)
+}
+
+pub enum MsgPrefix {
+    Name(String), // generic for when we don't know if a name is a nickname or a hostname - special case
+    NickHost(String, HostType),
+    NickUserHost(String, String, HostType),
+    Host(HostType)
+}
+
+pub struct ParseMsg {
+    prefix: Option<MsgPrefix>,
+    command: String,
+    // NB: our parser first makes a Vec<&str>, where things will still point to stuff
+    // in whatever the message slice sent to parse_message() was given a borrow of
+    // params could also be a &[String], or an explicit array of 15 Strings,
+    // but in the former case who owns the String array borrowed from?
+    params: Option<Vec<String>>
+}
+
+// parsing IRC messages :)
+// we want to use mostly &str operations for the parsing itself,
+// but we don't want to have to care about the fate of message,
+// so all the data structures we return will have new owned Strings
+// also changed my mind about CRLF checking, whoever calls this function will be
+// using CRLF delimiters to separate messages from the connection bytestream,
+// therefore, it doesn't make sense to check for them or for them to even be
+// included in the strings we get, same for empty lines - we shouldn't get any
+//    Augmented BNF notation for general message strcture
+//    message    =  [ ":" prefix SPACE ] command [ params ]
+pub fn parse_message(message: &str) -> Result<ParsedMsg, ParseError> {
+    // made get_prefix() code a bit nicer,
+    // get_prefix checks if there is a prefix or not,
+    // and returns both string slices as Option<&str>,
+    // to help handle the odd case where there's a prefix but no content
+    let (opt_prefix_string, opt_msg_body) = get_prefix(message);
+    let msg_body: &str = if let Some(substr) = opt_msg_body {
+        substr
+    } else {
+        // prefix string but no message body shouldn't really happen...
+        return Err(ParseError::NoCommand)
+    };
+
+    // let's handle the case here where we have a prefix string,
+    // now we want to parse it using the parse_prefix() function
+    let opt_prefix: Option<MsgPrefix> = if let Some(prefix_string) = opt_prefix_string {
+         match parse_prefix(prefix_string) {
+             Ok(val) => opt_prefix = Some(val),
+             Err(err_typ) => return Err(err_typ)
+         }
+    } else {
+        None
+    };
+    
+    // next we'll cut off the command part, that's fairly easy, we can index the first space and
+    // then cut off a slice, we can also stop at a special case here and leave the rest of the
+    // processing, if all we have is a command and no other parameters
+    let command: String;
+    let param_substring: &str;
+    match msg_body.find(' ') {
+        Some(index) => {
+            command = &body[..index].to_string();
+            if !rfc::is_valid_command(&command) {
+                return Err(ParseError::InvalidCommand);
+            }
+            param_substring = &body[index+1..];
+        }
+        None => {
+            command = body.to_string();
+            if !rfc::is_valid_command(&command) {
+                return Err(ParseError::InvalidCommand);
+            } else {
+                return Ok(ParseMsg {
+                    prefix: opt_prefix,
+                    command,
+                    params: None
+                });
+            }
+        }
+    }
+
+    // check for and split off the trailing argument
+    let (middle, opt_trail) = split_colon_arg(&param_substring);
+    let param_slices: Vec<&str>;
+    match opt_trail {
+        Some(trail_arg) => {
+            // how many spaces would we have for 15 parameters? 14 spaces,
+            // and if we have 15 parameters in *middle*, the last one has to
+            // swallow up trailing - so we used .splitn() on the whole of param_substring
+            if middle.split(' ').count() < rfc::MAX_MSG_PARAMS {
+                // in this case, however, we only splitn on the middle part
+                param_slices = middle.splitn(rfc::MAX_MSG_PARAMS - 1, ' ').collect();
+                param_slices.push(&trail_arg);
+            }
+        }
+        // this catches both the case of no trailing arg with a colon,
+        // and the case where there is a " :" found, but there are already too many params
+        _ => param_slices = param_substring.splitn(rfc::MAX_MSG_PARAMS, ' ').collect()
+    }
+
+    // now we've parsed them, but before giving them back to the caller, we want to copy everything
+    // from the string slices into some new Vec<String>, which we can pass ownership of along
+    let mut params: Vec<String> = Vec::new();
+    for i in param_slices.iter() {
+        params.push(i.to_string());
+    }
+
+    // return the stuff
+    Ok(ParseMsg {
+        opt_prefix,
+        command,
+        opt_params: Some(params)
+    })
+}
+
+// this'll do a splitn(2, ' '), then return the command,
+// plus optionally the rest of the message body, or None
+// if the command has no parameters at all (which is a valid case)
+// note one of the return values is a String! that's because
+// the caller wants one, and we also give up ownership, not a borrow
+fn get_command(msg_main: &str) -> (String, Option<&str>) {
+    let substrings: Vec<&str> = msg_main.splitn(2, ' ').collect();
+    match substrings.len() {
+        1 => (substrings[0].to_string(), None),
+        2 => (substrings[0].to_string(), Some(substrings[1])),
+        _ => panic!("unhandled exception, should be impossible")
+    }
+}
+
+fn get_prefix(message: &str) -> (Option<&str>, Option<&str>) {
+    // if we have a prefix, we will first have a colon indicator
+    // we know we will never have an empty line, but message.chars().nth(0) can give a
+    // Some(whatever) or a None, so we have to explicitly check that, or use a string slice
+    // this will panic if message is zero-length
+    if &message[..1] == ":" {
+        // check for a space
+        let substrings: Vec<&str> = (&message[1..]).splitn(2, ' ').collect();
+        match substrings.len() {
+            1 => (Some(substrings[0]), None),
+            2 => (Some(substrings[0]), Some(substrings[1])),
+            _ => panic!("unhandled exception, should be impossible though")
+        }
+    } else {
+        // no prefix found, so we just come back with only Some(message)
+        // but a None for the prefix option
+        (None, Some(message))
+    }
+}
+
+// parse the prefix part of an IRC message
+// with preceding colon and delimiting space stripped off
+fn parse_prefix(msg: &str) -> Result<MsgPrefix, ParseError> {
     // we should probably also return an error if the space occurs immediately after the colon
     if space_index == 0 {
     	return (msg, Err(ParseError::InvalidPrefix));
@@ -71,7 +221,6 @@ fn parse_prefix(msg: &str) -> Result<Box<irc::Source>, ParseError> {
     }
 }
     
-
 // this lil function snatches up a word and returns the rest of the string
 // in an Option<String>, or just gives back the original String plus a None
 fn split_colon_arg(msg: &str) -> (&str, Option<&str>) {
@@ -82,164 +231,3 @@ fn split_colon_arg(msg: &str) -> (&str, Option<&str>) {
     }
 }
 
-// parsing IRC messages :)
-// we'll also take ownership, calling function shouldn't need the original string anymore
-// irc::Message will contain both the Command and the Source (tho the latter is sometimes absent
-// we want to use mostly &str operations for the parsing itself,
-// but we don't want to have to care about the fate of message,
-// so all the data structures we return will have new owned Strings
-//    Augmented BNF notation for general message strcture
-//    message    =  [ ":" prefix SPACE ] command [ params ] crlf
-pub fn parse_message(mut message: &str) -> Result<ParsedMsg, ParseError> {
-    // make sure the message format is on point
-    if message.len() <= 2 {
-        return Err(ParseError::EmptyLine);
-    }
-
-    // deal with CRLF
-    let crlf = &message[message.len()-2..];
-    if crlf != "\r\n" {
-        return Err(ParseError::MissingCRLF);
-    }
-    message = &message[..message.len()-2];
-
-    // check for a message prefix
-    // should be bounded by a ':' at the very start of the string
-    // and a
-    let mut origin: Option<&irc::Source> = None;
-
-    if message.bytes().nth(0) == ':' {
-        // check for a space
-        if let Some(index) = message.find(' ') {
-            let prefix = &message[1..index];
-            if prefix.len() == 0 {
-                return Err(ParseError::InvalidPrefix);
-            }
-            message = &message[index+1..];
-            match parse_prefix(&prefix) {
-                Ok(val) => origin = Some(val),
-                Err(err) => return Err(err)
-            }
-        }
-    }
-    let (message, colon_arg) = split_colon_arg(&message);
-    let n = rfc::MAX_MSG_PARAMS + 1;
-    if let Some(arg) = colon_arg {
-            n -= 1;
-    }
-    let mut args: Vec<&str> = message.splitn(' ', n).collect();
-    if let Some(arg) = colon_arg {
-        args.push(arg);
-    }
-    let command = args.remove(0);
-
-    // command_type is an enum, but needs its parameters filled
-    // also need some checks for things like optional params
-    match command {
-        "JOIN" => {
-            // error if not enough args
-            if args.len() < 1 {
-                return Err(ParseError::TooFewArgs(command.to_string()));
-            }
-            
-            // anything else will be ignored, JOIN only needs a chan argument
-            // arg strings will be cloned before passing to Command::type(),
-            // otherwise we will have lifetime problems, and we can't move stuff
-            // from the args vector - this way args will cleanly go out of scope
-            let channel = args[0].to_string();
-
-            Ok(irc::Message {
-                cmd_params: Box::new(irc::Command::Join(channel)),
-                src: prefix
-            })
-        }
-        "MSG" => {
-            if args.len() < 2 {
-                return Err(ParseError::TooFewArgs(command.to_string()));
-            }
-            
-            let target = args[0].to_string();
-            let message = args[1].to_string();
-
-            Ok(irc::Message {
-                cmd_params: Box::new(irc::Command::Message(target, message)),
-                src: prefix
-            })
-        }
-        "PART" => {
-            // error if no chan given
-            if args.len() < 1 {
-                return Err(ParseError::TooFewArgs(command.to_string()));
-            }
-
-            let channel = args[0].to_string();
-            // anything in rest will be ignored, JOIN only needs a chan argument
-
-            // Option<String> is the expected type for Command::Part.part_message
-            let mut part_message: Option<String> = None;
-            if args.len() > 1 {
-                part_message = Some(args[1].to_string()); // don't forget to wrap the Option<T>s
-            }
-
-            Ok(irc::Message {
-                cmd_params: Box::new(irc::Command::Part(channel, part_message)),
-                src: prefix
-            })
-        }
-        "NICK" => {
-            // error if no nick given
-            if args.len() < 1 {
-                return Err(ParseError::TooFewArgs(command.to_string()));
-            }
-
-            let nick = args[0].to_string();
-
-            // prepare return struct
-            Ok(irc::Message {
-                cmd_params: Box::new(irc::Command::Nick(nick)),
-                src: prefix
-            })
-        }
-        "USER" => {
-            // total of 4 args required
-            if args.len() < 4 {
-                return Err(ParseError::TooFewArgs(command.to_string()));
-            }
-            
-            let username = args[0].to_string();
-
-            // not sure if silently ignoring non-numbers
-            // for the mode field is canonical behaviour,
-            // but whatever
-            let mode = match args[1].parse::<u32>() {
-                Ok(val) => val,
-                Err(_) => 0
-            };
-
-
-            // USER is specified as having an unused field, usually a * is supplied there
-            //let ignored = args[2]
-            let real_name = args[3].to_string();
-            
-            // prepare return struct
-            Ok(irc::Message {
-                cmd_params: Box::new(irc::Command::User(username, mode, real_name)),
-                src: prefix
-            })
-        }
-        "QUIT" => {
-            let quit_msg = if args.len() > 0 {
-                Some(args[0].to_string())
-            } else {
-                None
-            };
-            
-            // prepare return struct
-            Ok(irc::Message {
-                cmd_params: Box::new(irc::Command::Quit(quit_msg)),
-                src: prefix
-            })
-        }
-        _ => Err(ParseError::CommandNotRecognised(command.to_string()))
-    }
-}
