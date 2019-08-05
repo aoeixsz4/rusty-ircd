693fe9f9a6cc6ef07cae36ec9a1b603dc7ad82ef
Joanna I. Zaitseva-Kinneberg <jjadoyle@gmail.com>
2019-08-05 16:08:34 +0200

    fleshed out some of aspects of runtime data architecture in irc.rs, still not sure how this is going to play out with Rust's ownership rules - so far I've used IDs (chan IDs, user IDs, server IDs, client IDs) and HashMaps as an abstraction and no direct borrowing...

--- src/client.rs
+++ src/client.rs
@@ -1,39 +1,59 @@
-// irc::client
+// client
 // this file contains protocol defitions related to client commands coming to the server
+use crate::buffer;
+use crate::irc;
+use crate::parser;
+use std::net{IpAddr, Ipv4Addr, Ipv6Addr};
+
+pub enum ClientCommand {
+    Empty
+}
 
 pub struct Client { // is it weird/wrong to have an object with the same name as the module?
     // will need a hash table for joined channels
-    nick: String,
-    username: String,
     //channels: type unknown
-    socket: SocketType,
+    //socket: SocketType,
     //flags: some sort of enum vector?
-    inbuf: super::MessageBuffer,
-    outbuf: super::MessageBuffer,
+    host: irc::Host,
+    inbuf: buffer::MessageBuffer,
+    outbuf: buffer::MessageBuffer,
 }
 
-// externally, clients will probably be collected in a vector somewhere
+// externally, clients will probably be collected in a vector/hashmap somewhere
+// each client will have a unique identifier: their host (type irc::Host),
+// each must have a socket associated with it
+// clients here mean something associated with a socket connection -
+// i.e. they can be servers or users
+// somewhere we'll need code for mapping external users to whatever
+// relay server we can reach them through
 impl Client {
     // since new clients will be created on a new connection event,
     // we'll need a socket type as a parameter
-    pub fn new(socket: SocketType) -> Client {
+    // implementation decision: explicitly return as a pointer to heap data
+    // will also be necessary that all threads can access every client object
+    pub fn new(host: irc::Host) -> Client {
         Client {
-            mut nick: String::new(),
-            mut username: String::new(),
-            mut out_buffer: super::MessageBuffer,
-            mut in_buffer: super::MessageBuffer,
-            socket,
+            mut host,
+            mut outbuf: super::MessageBuffer::new(),
+            mut inbuf: super::MessageBuffer::new(),
+            //socket,
         }
     }
 
     // an event handler waiting on new data from the client
     // must call this handler when a CR-LF is found
     // return type is a ClientCommand, which will be processed elsewhere
-    pub fn end_of_line(&mut self) -> ClientCommand {
+    pub fn end_of_line(&mut self) -> Result<ClientCommand, parser::ParseError> {
         // NB: buffer index might not be directly after the CR-LF
         // first bit of code locates a CR-LF
         // next bit extracts a string and moves buffer data after CR-LF
         // to front, reseting the index afterwards
-        let command_string = self.out_buffer.extract();
+        let command_string = self.inbuf.extract_ln();
+
+        // i will insist that the event handler doesn't hand us empty lines
+        assert!(command_string.len() != 0);
+        let parsed_msg = parser::parse_message(command_string)?;
+
+        // do something with the parsed message, irc.rs code needs to get involved
     }
 }
