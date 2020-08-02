3e6398fcfb00a6998609a10bed1b653c35126a0b
aoei <jjadoyle@gmail.com>
2020-08-02 20:22:46 +0200

    change to Mutex in src/client.rs, make all lock() calls try_lock(), poll() expects a Vec<String> of replies to the client from irc::command()

--- src/client.rs
+++ src/client.rs
@@ -25,23 +25,9 @@ use crate::irc::Core;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use dns_lookup::lookup_addr;
 
-//pub struct ClientList {
-//    pub map: HashMap<u64, Arc<Mutex<Client>>>,
-//    pub next_id: u64
-//}
-//
-//impl ClientList {
-//    pub fn new() -> Self {
-//        ClientList {
-//            map: HashMap::new(),
-//            next_id: 0
-//        }
-//    }
-//}
-
 // this future is a wrapper to the Client struct, and implements the polling code
 pub struct ClientFuture {
-    pub client: Arc<RwLock<Client>>,
+    pub client: Arc<Mutex<Client>>,
     pub id: u64, // same as client id
     pub first_poll: bool,
     pub irc_core: Core
@@ -51,7 +37,7 @@ impl ClientFuture {
     // call when client connection drops (either in error or if eof is received)
     // remove client from list on EOF or connection error
     fn unlink_client(&mut self) {
-        let mut client_list = self.irc_core.clients.lock().unwrap();
+        let mut client_list = self.irc_core.clients.try_lock().unwrap().or_else(|_| { Ok(Async::NotReady) });
         // HashMap::remove() returns an Option<T>, so we can either
         // ignore the possibility that the client is alread unlinked, or deliberately panic
         // (since if this fails, there may well be a bug elsewhere
@@ -74,10 +60,9 @@ impl ClientFuture {
         // now we also have the slightly annoying situation that if bytes_out < out_i,
         // we have to either do someething complicated with two indices, or shift
         // bytes to the start of the buffer every time a write completes
+        let client = self.client.try_lock().unwrap().or_else(|_| { Ok(Async::NotReady) });
         let mut write_count: usize = 0;
         let mut tmp_buf: [u8; rfc::MAX_MSG_SIZE] = [0; rfc::MAX_MSG_SIZE];
-
-        let client = Arc::clone(&self.client).lock().unwrap();
         let len = client.output.copy(&mut tmp_buf); // returns bytes copied
 
         // write as much as we can while just incrementing indices
@@ -108,6 +93,8 @@ impl ClientFuture {
         // transfer it to the mutex guarded client.output buffer
         let mut tmp_buf: [u8; rfc::MAX_MSG_SIZE] = [0; rfc::MAX_MSG_SIZE];
         let mut tmp_index: usize = 0;
+        // ALL MUTEX LOCK ATTEMPTS TO BE NON-BLOCKING
+        let mut client = self.client.try_lock().unwrap().or_else(|_| { Ok(Async::NotReady) });
         while tmp_index < rfc::MAX_MSG_SIZE { // loop until there's nothing to read or the buffer's full
             match client.socket.poll_read(&mut tmp_buf[tmp_index ..]) {
                 Ok(Async::Ready(bytes_read)) if bytes_read == 0 =>
@@ -131,25 +118,6 @@ impl ClientFuture {
 
         Ok(tmp_index)
     }
-
-    // forward incoming message to other users
-    fn broadcast(&self, map: &HashMap<u64, Weak<RwLock<Client>>>, msg: &str) {
-        for (id, target) in map {
-            // skip writing to ourself
-            if *id == self.id {
-                continue;
-            }
-            
-            let target_ptr = Weak::upgrade(target).unwrap();
-            let mut target_wl = target_ptr.lock().unwrap();
-
-
-            // send_line() takes care of notifying the Future's task and flags
-            // the client as dead if the append fails (indicates full buffer
-            // (indicates flushes are not successful))
-            target_wl.send_line(&msg);
-        }
-    }
 }
 
 impl Future for ClientFuture {
@@ -157,35 +125,25 @@ impl Future for ClientFuture {
     type Error = ();
     // this here is the main thing
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        {
-            // make a mini-scope to minimise locks on client
-            let client_arc = Arc::clone(&self.client);
-            let client_ro = client_arc.lock().unwrap();
-            // is connection/client dead? drop from list and return Ready to complete our future
-            if client_ro.dead == true {
-                self.unlink_client(&client_ro);
-                return Ok(Async::Ready(()));
-            }
+        // is connection/client dead? drop from list and return Ready to complete our future
+        if self.client.try_lock().unwrap()
+            .or_else(|_| { Ok(Async::NotReady) }).dead() {
+            self.unlink_client(&client);
+            return Ok(Async::Ready(()));
         }
 
         // if its the first time polling, we need to register our task
         if self.first_poll == true {
             self.first_poll = false;
-            let client_arc = Arc::clone(&self.client);
-            let client_rw = client_arc.lock().unwrap();
-            client_rw.handler = task::current();
+            self.client.try_lock().unwrap()
+                .or_else(|_| { Ok(Async::NotReady) }).set_handler(task::current());
         }
 
         // try to write if there is anything in outbuf,
         // returns error if there is a connection problem, in which case drop the client
-        {
-            // make a mini-scope to minimise locks on client
-            let client_arc = Arc::clone(&self.client);
-            let client_ro = client_arc.lock().unwrap();
-	        if let Err(_e) = self.try_flush() {
-                self.unlink_client();
-                return Ok(Async::Ready(()));
-            }
+	    if let Err(_e) = self.try_flush() {
+            self.unlink_client();
+            return Ok(Async::Ready(()));
         }
 
         // try to read into our client's in-buffer
@@ -194,26 +152,33 @@ impl Future for ClientFuture {
             return Ok(Async::Ready(()));
         }
 
-        {
-            // make a mini-scope to minimise locks on client
-            let client_arc = Arc::clone(&self.client);
-            let client_ro = client_arc.lock().unwrap();
-            // loop while client's input buffer contains line delimiters
-            while client_ro.input.has_delim() {
-                let msg_string = client_ro.input.extract_ln();
-                assert!(msg_string.len() != 0);
-                let parsed_msg_res = parser::parse_message(&msg_string);
-                match parsed_msg_res {
-                    Ok(msg) => irc::command(Arc::clone(&self.irc_core), Arc::clone(&client_ro), msg),
-                    Err(parse_err) => match parse_err {
-                        ParseError::InvalidPrefix => client_ro.send_line("invalid prefix!"),
-                        ParseError::NoCommand => client_ro.send_line("no command given!"),
-                        ParseError::InvalidCommand => client_ro.send_line("invalid command!"),
-                        ParseError::InvalidNick => client_ro.send_line("invalid nick!"),
-                        ParseError::InvalidUser => client_ro.send_line("invalid user!"),
-                        ParseError::InvalidHost => client_ro.send_line("invalid host!")
-                    }
+        // loop while client's input buffer contains line delimiters
+        while self.client.try_lock().unwrap()
+            .or_else(|_| { Ok(Async::NotReady) }).has_delim() {
+            let msg_string = self.client.try_lock().unwrap()
+                .or_else(|_| { Ok(Async::NotReady) }).input.extract_ln();
+            assert!(msg_string.len() != 0);
+
+            // let's collect responses to send to *this* client here,
+            // then irc.rs only needs to worry about forwarding stuff
+            let responses = parser::parse_message(&msg_string) {
+                Ok(msg) => irc::command(Arc::clone(&self.irc_core), Arc::clone(&self.client), msg),
+                Err(parse_err) => {
+                    let resp = Vec::new();
+                    resp.push(parser::err_to_msg(parse_err));
+                    resp
                 }
+            }
+
+            for response in responses {
+                /* NOTICE: lossy behaviour on error, if
+                   the lock *does* fail, responses meant
+                   for the client will be dropped, so
+                   we may want to consider a way to spawn
+                   ClientSend Futures... */
+                self.client.try_lock().unwrap()
+                    .or_else(|_| { Ok(Async::NotReady) })
+                    .send_line(response);
             }
         }
 
@@ -270,12 +235,14 @@ impl Client {
     // so just an undelimited line should be passed as a &str
     // the function also notifies the runtime that the socket handler needs
     // to be polled to flush the write
-    pub fn send_line(&mut self, buf: &str) {
-        let mut string = buf.to_string();
-        string.push_str("\r\n");
+    pub fn send_line(&mut self, line: &str) {
         self.handler.notify();
-        if let Err(_e) = self.output.append_str(&string) {
+        if let Err(_e) = self.output.append_ln(&string) {
             self.dead = true;
         }
+    }
+
+    pub fn has_delim(&self) -> bool {
+        self.input.has_delim()
     }
 }
